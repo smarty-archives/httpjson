e90b529fdc7354ef7f5906944471d256bdad78da
Jonathan Oliver <jonathan.s.oliver42@gmail.com>
2020-11-24 09:06:26 -0700

    Consistently in receiver names.

--- render_options.go
+++ render_options.go
@@ -7,24 +7,24 @@ type RenderOption func(*ResponseRenderer)
 type options struct{}
 
 func (options) RenderIndent(prefix, indent string) RenderOption {
-	return func(renderer *ResponseRenderer) {
-		renderer.Prefix = prefix
-		renderer.Indent = indent
+	return func(this *ResponseRenderer) {
+		this.Prefix = prefix
+		this.Indent = indent
 	}
 }
 func (options) RenderStatusCode(code int) RenderOption {
-	return func(renderer *ResponseRenderer) {
-		renderer.StatusCodeValue = code
+	return func(this *ResponseRenderer) {
+		this.StatusCodeValue = code
 	}
 }
 func (options) RenderHeader(key string, values ...string) RenderOption {
-	return func(renderer *ResponseRenderer) {
-		renderer.AddHeader(values, key)
+	return func(this *ResponseRenderer) {
+		this.AddHeader(values, key)
 	}
 }
 func (options) RenderData(data interface{}) RenderOption {
-	return func(renderer *ResponseRenderer) {
-		renderer.Data = data
+	return func(this *ResponseRenderer) {
+		this.Data = data
 	}
 }
 func (options) RenderMappedErrors(mapper func(error) Error, errs ...error) RenderOption {
